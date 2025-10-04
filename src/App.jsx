59d5055264d9a93dd6d0a67de5885a61340bd1ac
Tavan Thiry <tavanthiry@Arilds-Mac-mini.local>
2025-10-04 10:31:01 +0200

    Add Nav component and NotFound route

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { PopularMovies } from './pages/PopularMovies'
 import { NewMovies } from './pages/NewMovies'
 import { MovieDetails } from './pages/MovieDetails'
+import { Nav } from './components/Nav'
+import { NotFound } from './pages/NotFound'
 
 export const App = () => {
   return (
     <main>
       <BrowserRouter>
+        <Nav />
           <Routes>
             <Route path="/" element={<PopularMovies />} />
             <Route path="/news" element={<NewMovies />} />
