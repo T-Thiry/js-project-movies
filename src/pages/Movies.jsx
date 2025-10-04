27c2153beb7a1adc13c77351e45ab112e316429e
Tavan Thiry <tavanthiry@Arilds-Mac-mini.local>
2025-10-04 13:28:54 +0200

    Create basic Movies page with placeholder title

---
+++ src/pages/Movies.jsx
@@ -0,0 +1,14 @@
+import React, { use } from 'react'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  padding: 1rem 2rem;
+  `
+
+  export const Movies = () => {
+  return (
+    <div>
+      <Title>Movies</Title>
+    </div>
+  )
+}
