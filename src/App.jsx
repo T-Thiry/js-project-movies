7989f9b8bbec59aeffa6eb3c50d16b480619d4a1
Tavan Thiry <tavanthiry@Arilds-Mac-mini.local>
2025-10-04 10:26:57 +0200

    Add routes for PopularMovies, NewMovies and MovieDetails

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { PopularMovies } from './pages/PopularMovies'
+import { NewMovies } from './pages/NewMovies'
+import { MovieDetails } from './pages/MovieDetails'
 
 export const App = () => {
   return (
     <main>
       <BrowserRouter>
           <Routes>
+            <Route path="/" element={<PopularMovies />} />
+            <Route path="/news" element={<NewMovies />} />
+            <Route path="/movie:id" element={<MovieDetails />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
       </BrowserRouter>
     </main>
