be27bb290fbeae8752babe5a901c76ce59726a37
Tavan Thiry <tavanthiry@Arilds-Mac-mini.local>
2025-10-05 13:34:04 +0200

    Add content and stying to NotFound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,14 +1,31 @@
-import React, { use } from 'react'
 import styled from 'styled-components'
 
+const NotFoundContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  text-align: center;
+  background-color: #333;
+  color: white;
+  `
+
 const Title = styled.h1`
-  padding: 1rem 2rem;
+  font-size: 3rem;
+  margin-bottom: 1rem;
+  `
+
+const Message = styled.p`
+  font-size: 1.5rem;
+  margin-bottom: 2rem;
   `
 
   export const NotFound = () => {
   return (
-    <div>
-      <Title>Not Found</Title>
-    </div>
+    <NotFoundContainer>
+      <Title>404 - Page Not Found</Title>
+      <Message>Sorry, the page you are looking for does not exist. Go back to Movies</Message>
+    </NotFoundContainer>
   )
 }
